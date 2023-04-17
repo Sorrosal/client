e29fb2426afd6db4dce4ae16c9de5a6838433333
Sorrosal <sergiosorrosalgayan@gmail.com>
2023-04-17 11:49:24 +0200

    Sistema de autenticación

--- src/components/Admin/Auth/LoginForm/LoginForm.js
+++ src/components/Admin/Auth/LoginForm/LoginForm.js
@@ -3,10 +3,11 @@ import { Form } from "semantic-ui-react";
 import { useFormik } from "formik";
 import { Auth } from "../../../../api";
 import { initialValues, validationSchema } from "./LoginForm.form";
-
+import { useAuth } from "../../../../hooks";
 const authController = new Auth();
 
 export  function LoginForm() {
+    const { login } = useAuth();
     const formik = useFormik({
         initialValues: initialValues(),
         validationSchema: validationSchema(),
@@ -14,9 +15,11 @@ export  function LoginForm() {
         onSubmit: async (formValue) => {
             try{
                 const response = await authController.login(formValue);
-                console.log(response);
+                authController.setAccessToken(response.access);
+                authController.setRefreshToken(response.refresh);
+                login(response.access);
             }catch (error) {
-                console.log(error);
+                console.error(error);
             }
         }
     })
--- src/router/AdminRouter.js
+++ src/router/AdminRouter.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import {AdminLayout} from "../layouts";
 import { Auth, Users, Blog, Courses, Menu, Newsletter } from "../pages/admin";
+import { useAuth } from "../hooks";
 
-const user = null;
+
 
 export  function AdminRouter() {
 
+  const { user } = useAuth();
   
   const loadLayout = (Layout, Page) => {
     return(
@@ -22,9 +24,9 @@ export  function AdminRouter() {
         <Route path="/admin/*" element={<Auth/>}/>
       ) : (
         <>
-        {["/admin", "/admin/blog"].map((path) => {
+        {["/admin", "/admin/blog"].map((path) => (
           <Route key={path} path={path} element={loadLayout(AdminLayout, Blog)} />
-        })}
+        ))}
         <Route path="/admin/users" element={loadLayout(AdminLayout, Users)} />
         <Route path="/admin/courses" element={loadLayout(AdminLayout, Courses)} />
         <Route path="/admin/menu" element={loadLayout(AdminLayout, Menu)} />
